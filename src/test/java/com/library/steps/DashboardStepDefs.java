3a68cc94200dae8c3aae3d382f386b66c6300936
NurWedebrand <aysenurmercan98@gmail.com>
2025-08-22 12:53:00 -0500

    Dashboard tests added.

---
+++ src/test/java/com/library/steps/DashboardStepDefs.java
@@ -0,0 +1,20 @@
+package com.library.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DashboardStepDefs {
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String string) {
+
+    }
+    @When("the user gets users, books, borrowed books numbers from UI")
+    public void the_user_gets_users_books_borrowed_books_numbers_from_ui() {
+
+    }
+    @Then("the information should be the same with the database")
+    public void the_information_should_be_the_same_with_the_database() {
+
+    }
+}
---
+++ src/test/resources/features/dashboardTestNur.feature
@@ -0,0 +1,6 @@
+@now
+Feature: Dashboard Test
+Scenario: Dashboard data verification
+Given the user logged in as "librarian"
+When the user gets users, books, borrowed books numbers from UI
+Then the information should be the same with the database
