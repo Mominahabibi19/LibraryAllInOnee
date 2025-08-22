4497398e87d92a7420a20844ff53f296391e7940
muus <nadireulsular@gmail.com>
2025-08-22 13:57:44 -0400

    dashboard tests added.

--- src/test/java/com/library/steps/DashboardSD.java
+++ src/test/java/com/library/steps/DashboardSD.java
@@ -8,7 +8,7 @@ public class DashboardSD {
 
     @Given("the user logged in as {string}")
     public void the_user_logged_in_as(String string) {
-
+    // test
     }
     @When("the user gets users, books, barrowed books numbers from UI")
     public void the_user_gets_users_books_barrowed_books_numbers_from_ui() {
